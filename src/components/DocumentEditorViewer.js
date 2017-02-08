eb71f53f04710994aef3bdd1bd9357e9d569d708
John Munson <johncmunson91@gmail.com>
2017-02-08 16:07:35 -0700

    installed react-tabs and integrated into DocumentEditorView.js

--- src/components/DocumentEditorViewer.js
+++ src/components/DocumentEditorViewer.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import DocumentViewer from './DocumentViewer.js'
 import DocumentEditor from './DocumentEditor.js'
+import Tabs from './Tabs.js'
 import { Page, Row, Column } from 'hedron'
 
 const DocumentEditorViewer = (props) => (
     <Page>
+        <Row debug>
+            <Column sm={12} md={12} lg={12}>
+                <Tabs {...props} />
+            </Column>
+        </Row>
         <Row debug>
             <Column sm={6} md={6} lg={6}>
                 <DocumentEditor {...props} />
