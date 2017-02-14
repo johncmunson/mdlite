2d50edb6dfbd84adfbe319a59faf320326a12646
John Munson <johncmunson91@gmail.com>
2017-02-14 13:20:19 -0700

    added redux-logger and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store/store.js'
+import configureStore from './store/configureStore.js'
 import App from './App'
 import './index.css'
+
+const store = configureStore()
 
 render(
     <Provider store={store}>
--- src/store/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'
-import rootReducer from '../reducers/rootReducer.js'
-
-const store = createStore(rootReducer)
-
-export default store
